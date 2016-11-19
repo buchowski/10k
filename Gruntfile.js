94aa521897df2edfd6d7f694eefaf75983b62602
buchowski <bucholz.adam@gmail.com>
2016-11-19 10:56:25 -0600

    minify css. do not track /dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,11 +13,20 @@ module.exports = function (grunt) {
 				files: ['src/styles.less'],
 				tasks: ['less:dev']
 			}
+		},
+		cssmin: {
+			target: {
+				files: {
+					'dist/styles.min.css': 'dist/styles.css'
+				}
+			}
 		}
 	})
 
 	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-watch')
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 
 	grunt.registerTask('default', ['less:dev', 'watch:dev']);
+	grunt.registerTask('production', ['less:dev', 'cssmin'])
 };
